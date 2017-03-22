a83253a29d764faa18d9790a7f3181670a9017ad
Laura Turk <laura.lk.turk@gmail.com>
2017-03-22 14:09:09 -0600

    Refactor lane and frog tests. Need a way to test for keypress

--- test/frog-test.js
+++ test/frog-test.js
@@ -34,59 +34,50 @@ describe ('Frog', function() {
       assert.isFunction(frog.draw);
     });
 
-    it('should have a function called "moveUp"', function () {
-      assert.isFunction(frog.moveUp);
-    });
+    it('should have a function called "move()"', function () {
+      assert.isFunction(frog.move);
+    })
 
-    it('should move up equal to its height', function () {
-      frog.moveUp();
-      assert.equal(frog.y, 450);
-    });
-
-    it('should have a function called "moveDown"', function () {
-      assert.isFunction(frog.moveDown);
-    });
-
-    it('should move down equal to its height', function () {
-      frog.moveUp();
-      frog.moveDown();
-      assert.equal(frog.y, 450);
-    });
-
-    it('should have a function called "moveRight"', function () {
-      assert.isFunction(frog.moveRight);
-    });
-
-    it('should move right equal to its width', function () {
-      frog.moveRight();
-      assert.equal(frog.x, 275);
-    });
-
-
-    it('should not move past the canvas end', function () {
-      for (let i = 0; i < 10; i++) {
-        frog.moveRight()
-      }
-      assert.equal(frog.x, 475);
-    });
-
-    it('should have a function called "moveLeft"', function () {
-      assert.isFunction(frog.moveLeft);
-    });
-
-    it('should move left equal to its width', function () {
-			let frog2 = new Frog ();
-      frog2.moveLeft();
-      assert.equal(frog2.x, 225);
-    });
-
-    it('should not move past the canvas end moving left', function () {
-			let frog3 = new Frog ();
-			for (let i = 0; i < 10; i++) {
-        frog3.moveLeft();
-      }
-      assert.equal(frog3.x, 0);
-    });
+    // it('should move up equal to its height', function (e) {
+    //   e.keyCode = 38
+    //   frog.move(e);
+    //   assert.equal(frog.y, 450);
+    // });
+    //
+    //
+    // it('should move down equal to its height', function () {
+    //   frog.move(38);
+    //   frog.move(40);
+    //   assert.equal(frog.y, 450);
+    // });
+    //
+    //
+    // it('should move right equal to its width', function () {
+    //   frog.move(39);
+    //   assert.equal(frog.x, 275);
+    // });
+    //
+    //
+    // it('should not move past the canvas end', function () {
+    //   for (let i = 0; i < 10; i++) {
+    //     frog.move(39)
+    //   }
+    //   assert.equal(frog.x, 475);
+    // });
+    //
+    // it('should move left equal to its width', function () {
+		// 	let frog2 = new Frog ();
+    //   frog2.move(37);
+    //   assert.equal(frog2.x, 225);
+    // });
+    //
+    // it('should not move past the canvas end moving left', function () {
+		// 	let frog3 = new Frog ();
+		// 	for (let i = 0; i < 10; i++) {
+    //     frog3.move(37);
+    //   }
+    //   assert.equal(frog3.x, 0);
+    // });
 
     it('should instantiate at the bottom center of the canvas', function () {
 			let frog4 = new Frog ();
@@ -121,17 +112,9 @@ describe ('Frog', function() {
 		//
     // });
 
-    it('should have a function called "winCollide()" ', function () {
-      assert.isFunction(frog.winCollide);
-    });
 
     it('should have a function called "death()" ', function () {
       assert.isFunction(frog.death);
-    });
-
-    it('should turn red upon dying', function () {
-      frog.death();
-      assert.equal(frog.color, 'rgba(250, 0, 0, 1)');
     });
 
     it('should return to the starting place upon dying', function () {
