73951b1fe356ccff8708b23f00ea9b2617345dd7
Devin Dixon <devinisafoal@gmail.com>
2017-03-22 10:43:46 -0600

    finish tests for world

--- test/lilypad-test.js
+++ test/lilypad-test.js
@@ -19,11 +19,10 @@ describe('Lilypad', () => {
     assert.equal(lilypad1.x, 50);
   });
 
-  it('should have a y, height, width, and color', () => {
+  it('should have a y, height, and width', () => {
     assert.equal(lilypad.y, 0);
     assert.equal(lilypad.height, 50);
     assert.equal(lilypad.width, 50);
-    assert.equal(lilypad.color, 'rgba(8, 7, 143, 1)');
   });
 
   it('should have a function called draw', () => {
